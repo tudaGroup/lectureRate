ee5f74853b42fed9239b27e8559c0bf09901680c
PeanutI3utter <sijunkwon@gmail.com>
2020-10-20 15:04:59 +0200

    UserAccount class defined for database use

---
+++ src/main/java/com/tudagroup/lecturerate/db/model/Degree.java
@@ -0,0 +1,11 @@
+package com.tudagroup.lecturerate.db.model;
+
+public enum Degree {
+    BachelorOfScience,
+    BachelorOfArts,
+    BachelorOfEducation,
+    JointBachelorOfArts,
+    MasterOfScience,
+    MasterOfArts,
+    MasterOfEducation
+}
